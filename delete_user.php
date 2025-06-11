8cce9b5f483b8b37bc04190e5180455a33d29d23
mahajar <hajar17maacha@gmail.com>
2025-06-11 08:39:39 +0100

    Create delete_user.php

---
+++ delete_user.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'config.php';
+
+$id = $_GET['id'];
+$stmt = $pdo->prepare('DELETE FROM users WHERE id = ?');
+$stmt->execute([$id]);
+
+header('Location: index.php');
+exit;
+?>
